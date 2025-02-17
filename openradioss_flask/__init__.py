004e86eba5cb31c55151621525b72bd955eaa2ae
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-02-17 23:58:52 +0100

    update

--- openradioss_flask/__init__.py
+++ openradioss_flask/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import webview
 
 
 app = Flask(__name__)
+window = webview.create_window('OpenRadioss', app)
 
 @app.route("/")
 def hello_world():
--- run.py
+++ run.py
@@ -1,5 +1,21 @@
-from openradioss import app
+from openradioss_flask import window
+
+import webview
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    try:
+        import pyi_splash
+
+        # Update the text on the splash screen
+        pyi_splash.update_text("PyInstaller is a great software!")
+        pyi_splash.update_text("Second time's a charm!")
+
+        # Close the splash screen. It does not matter when the call
+        # to this function is made, the splash screen remains open until
+        # this function is called or the Python program is terminated.
+        pyi_splash.close()
+    except:
+        pass
+
+    webview.start()
