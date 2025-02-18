73b43a2335314451231d93b5c1136e50745392db
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-02-18 12:08:18 +0100

    Update create_dist.bat

--- create_dist.bat
+++ create_dist.bat
@@ -1,1 +1,7 @@
+@set mypath=%cd%
+
+cd %mypath%
+
 pyinstaller run.py --icon "openradioss_flask/static/ross.ico" --add-data="openradioss_flask/templates/*:openradioss_flask/templates" --add-data="openradioss_flask/static/*:openradioss_flask/static" --add-data="openradioss_flask/data/*:openradioss_flask/data" --add-data="LICENSE:." --noconsole --splash openradioss_flask/static/openradioss-splash.jpg --noconfirm
+
+@Pause
