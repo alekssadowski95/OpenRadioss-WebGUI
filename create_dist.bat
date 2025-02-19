f7d64c2dad49c1eb6ddf078b75d0a05e5910b362
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2025-02-19 09:43:20 +0100

    update

--- create_dist.bat
+++ create_dist.bat
@@ -2,6 +2,6 @@
 
 cd %mypath%
 
-pyinstaller run.py --icon "openradioss_flask/static/ross.ico" --add-data="openradioss_flask/templates/*:openradioss_flask/templates" --add-data="openradioss_flask/static/*:openradioss_flask/static" --add-data="openradioss_flask/data/*:openradioss_flask/data" --add-data="LICENSE:." --noconsole --splash openradioss_flask/static/openradioss-splash.jpg --noconfirm
+python3 -m PyInstaller run.py --icon "openradioss_flask/static/ross.ico" --add-data="openradioss_flask/templates/*:openradioss_flask/templates" --add-data="openradioss_flask/static/*:openradioss_flask/static" --add-data="openradioss_flask/data/*:openradioss_flask/data" --add-data="LICENSE:." --noconsole --splash openradioss_flask/static/openradioss-splash.jpg --noconfirm
 
 @Pause
--- openradioss_flask/__init__.py
+++ openradioss_flask/__init__.py
@@ -3,6 +3,7 @@ from flask import render_template
 
 
 PATH_TO_OPENRADIOSS_DIR = "../OpenRadioss/"
+INPUT_FILE = None
 
 app = Flask(__name__)
 
@@ -13,3 +14,7 @@ def home():
 @app.route("/vtk")
 def vtk():
     return render_template('vtk.html')
+
+@app.route("/set-inp-file")
+def set_inp_file():
+    return render_template('inp-upload.html')
